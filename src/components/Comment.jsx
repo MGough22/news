89147a8a971f8db41742728212066658a1d2c765
MGough 22 <gentianloom@gmail.com>
2024-12-18 23:53:01 +0000

    added avatar rendering

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,14 +1,61 @@
+import { useEffect, useState } from "react";
 import { formatCreatedAtDate } from "../utils/formatDate";
+import { hatch } from "ldrs";
+
+hatch.register();
 export const Comment = ({ commentObject }) => {
-  return (
-    <div className="comment-instance">
-      <p className="comment-info">
-        {commentObject.author} | at{" "}
-        {formatCreatedAtDate(commentObject.created_at)}
-      </p>
-      <hr></hr>
-      <p className="comment-body">{commentObject.body}</p>
-      <p className="comment-info">votes : {commentObject.votes}</p>
-    </div>
-  );
+  const [userData, setUserData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    console.log(commentObject.author);
+    const fetchUserData = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `https://res-borealis.onrender.com/api/users/${commentObject.author}`
+        );
+        if (!response.ok) {
+          throw new Error(
+            `Failed to fetch user image, error type: ${response.status}`
+          );
+        }
+        const responseData = await response.json();
+        setUserData(responseData);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchUserData();
+  }, []);
+
+  if (isLoading)
+    return (
+      <div className="loading-anim">
+        <l-hatch size="150" stroke="4" speed="3.5" color="black"></l-hatch>
+      </div>
+    );
+  if (error) return <p>Error: {error}</p>;
+
+  if (!isLoading) {
+    return (
+      <div className="comment-instance">
+        <img
+          className="user-avatar"
+          src={userData.user.avatar_url}
+          alt={`Avatar of ${userData.user.username}`}
+        />
+        <p className="comment-info">{commentObject.author}</p>
+        <p className="comment-info">
+          {formatCreatedAtDate(commentObject.created_at)}
+        </p>
+        <hr></hr>
+        <p className="comment-body">{commentObject.body}</p>
+        <p className="comment-info">votes : {commentObject.votes}</p>
+      </div>
+    );
+  }
 };
--- src/components/SingleArticleView.jsx
+++ src/components/SingleArticleView.jsx
@@ -51,7 +51,6 @@ export const SingleArticleView = () => {
     );
   if (error) return <p>Error: {error}</p>;
   if (!isLoading) {
-    // console.log(comments[0]);
     return (
       <div className="article-and-comments">
         <div className="single-article-view">
@@ -83,15 +82,3 @@ export const SingleArticleView = () => {
     );
   }
 };
-
-// "exampleResponse": {
-//     "article_id": 11,
-//     "title": "Am I a cat?",
-//     "topic": "mitch",
-//     "author": "icellusedkars",
-//     "body": "Having run out of ideas for articles, I am staring at the wall blankly, like a cat. Does this make me a cat?",
-//     "created_at": "2020-01-15T22:21:00.000Z",
-//     "votes": 0,
-//     "article_img_url": "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
-//     "comment_count": 0
-//   }
